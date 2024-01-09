211d244cad21801f717ea78d4177afe06d8848a2
SmartBigGuru <smartbigguru@gmail.com>
2024-01-09 15:15:57 -0500

    update frontend

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-const path = require('path')
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = {
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-
-    return config
-  }
-}
+module.exports = nextConfig
